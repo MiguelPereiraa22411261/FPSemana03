484e00c84a777973afeb24d9d79f829f1a296502
MiguelPereiraa22411261 <a22411261@alunos.ulht.pt>
2024-11-22 13:42:37 +0000

    Finished queue_process

--- queue_process.py
+++ queue_process.py
@@ -3,7 +3,7 @@ from collections import deque
 inputed = input()
 words = inputed.split()
 queue = deque(words)
-print(queue)
+print(f"deque({list(queue)[::-1]})")
 while queue:
     word = queue.pop()
     if "o" in word:
